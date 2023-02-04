576c95864ccfb37c4dee76805c8a52f001ba7556
h-amini1 <amini.hassan27@gmailcom>
2023-02-04 12:09:58 -0600

    added versioninig file

---
+++ sesson-2/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.3.7" # Terraform Version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.53.0"  # AWS Provider
+    }
+  }
+}
+
+
+# Terraform = v1.3.7
+# AWS Provider = v4.53.0
+
+# Semantic Versioning
+# v1.3.7

32d6129d09dba0eafcdcf337ce0383207246d648
h-amini1 <amini.hassan27@gmailcom>
2023-02-06 20:06:07 -0600

    Referances

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,4 +1,24 @@
 resource "aws_instance" "main" {
   ami           = "ami-0036b4598ccd42565"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["aws_security_group.main_sg.id"] # a list of strings
 }
+
+
+# What is list? 
+# Fruit = [apple, orange, banana]
+#car   = [lexus, BMW, Mercedes, Honda]
+
+#types:
+#1. Integer
+#2. Float
+#3. Number = 0.1.2.3.
+#4. string = "" # all characters, whatever you see on your keyboard ""
+#5. Boolean = true, false
+
+
+# Reference to resource?
+# 1. First labal, 
+# 2. Second lable
+# 3, 
+# How to reference and declear and ID 

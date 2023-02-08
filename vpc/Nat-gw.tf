97c714ae5f2e35813752902180533f56cbb18c8b
h-amini1 <amini.hassan27@gmailcom>
2023-02-08 17:41:07 -0600

    commented natgw

--- vpc/Nat-gw.tf
+++ vpc/Nat-gw.tf
@@ -1,12 +1,12 @@
-resource "aws_nat_gateway" "task_ngw" {
-  allocation_id = aws_eip.nat_gateway_eip.id # Asking for Elastic IP
-  subnet_id     = aws_subnet.pub_sub_b.id
+# resource "aws_nat_gateway" "task_ngw" {
+#   allocation_id = aws_eip.nat_gateway_eip.id # Asking for Elastic IP
+#   subnet_id     = aws_subnet.pub_sub_b.id
 
-  tags = {
-    Name = "NATgw"
-  }
+#   tags = {
+#     Name = "NATgw"
+#   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
   
-}
+# }

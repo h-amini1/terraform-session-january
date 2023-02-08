f3865168f78fa2a70cdbc7b7db423a9ce2556e86
h-amini1 <amini.hassan27@gmailcom>
2023-02-08 16:30:05 -0600

    add subnet

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "pub_sub_b" {
 
 resource "aws_subnet" "private_sub_a" {
   vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.11.0/24"
   availability_zone = "us-west-1a"
   tags = {
     Name = "private_sub_a"
@@ -27,7 +27,7 @@ resource "aws_subnet" "private_sub_a" {
 
 resource "aws_subnet" "private_sub_b" {
   vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.12.0/24"
   availability_zone = "us-west-1b"
   tags = {
     Name = "private_sub_b"

a410b318b04ec0d78183091f2e6740396bcd4b20
h-amini1 <amini.hassan27@gmailcom>
2023-02-08 17:27:08 -0600

    add natgw

--- vpc/Nat-gw.tf
+++ vpc/Nat-gw.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "task_ngw" {
-  allocation_id = aws_eip.eip.allocationid # Asking for Elastic IP
+  allocation_id = aws_eip.nat_gateway_eip.id # Asking for Elastic IP
   subnet_id     = aws_subnet.pub_sub_b.id
 
   tags = {
@@ -8,5 +8,5 @@ resource "aws_nat_gateway" "task_ngw" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  
 }

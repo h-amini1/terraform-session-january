f74db85bc6ad0ec36444bffb762ef718883dd3da
h-amini1 <amini.hassan27@gmailcom>
2023-02-09 00:02:35 -0600

    ngw

--- vpc/Nat-gw.tf
+++ vpc/Nat-gw.tf
@@ -2,7 +2,6 @@
 resource "aws_nat_gateway""homework_nat_gateway"{
     subnet_id = aws_subnet.aws_subnet.pub_sub_b.id
     connectivity_type = "public"
-    allocation_id = aws_eip.nat_gateway_eip.id
     tags ={
         Name = "nat_gateway_name"
     }

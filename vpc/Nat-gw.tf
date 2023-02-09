9c94f13fa5852cff7d8bf4109c7253dab2cb10a8
h-amini1 <amini.hassan27@gmailcom>
2023-02-08 23:49:08 -0600

    ngw

--- vpc/Nat-gw.tf
+++ vpc/Nat-gw.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "task_nat_gw" {
   allocation_id = aws_eip.vpc_eip.allocation_id
-  subnet_id     = aws_subnet.pub_sub_a.id
+  subnet_id     = aws_subnet.public_sub_b.id
 
   tags = {
     Name = "terra_nat_gw"

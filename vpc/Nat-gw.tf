07a8c1063d9800db5680ccfe6ea54fcf37e4460a
h-amini1 <amini.hassan27@gmailcom>
2023-02-08 23:58:38 -0600

    ngw

--- vpc/Nat-gw.tf
+++ vpc/Nat-gw.tf
@@ -1,9 +1,9 @@
-resource "aws_nat_gateway" "task_nat_gw" {
-  allocation_id = aws_eip.vpc_eip.allocation_id
-  subnet_id     = aws_subnet.aws_subnet.pub_sub_b.id
 
-  tags = {
-    Name = "terra_nat_gw"
-  }
-
+resource "aws_nat_gateway""homework_nat_gateway"{
+    subnet_id = aws_subnet.aws_subnet.pub_sub_b.id
+    connectivity_type = "public"
+    allocation_id = aws_eip.nat_gateway_eip.id
+    tags ={
+        Name = "nat_gateway_name"
+    }
 }

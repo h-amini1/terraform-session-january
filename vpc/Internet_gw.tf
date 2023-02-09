696a9eb3bedafc0c56248240b483a71ab2817e9c
h-amini1 <amini.hassan27@gmailcom>
2023-02-08 18:11:04 -0600

    Internet-gw

--- vpc/Internet_gw.tf
+++ vpc/Internet_gw.tf
@@ -1,8 +1,7 @@
-resource "aws_internet_gateway" "Internet-gw" {
+resource "aws_internet_gateway" "task_int_gw" {
   vpc_id = aws_vpc.task_vpc.id
-}
 
-    tags = {
-    Name = "task_ig"
-
+  tags = {
+    Name = "terra_internet_gw"
+  }
 }

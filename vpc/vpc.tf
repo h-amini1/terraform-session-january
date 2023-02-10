52730dc10df3ad15546c9df010eaaec171a436d2
h-amini1 <amini.hassan27@gmailcom>
2023-02-09 21:12:43 -0600

    nat

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -72,13 +72,12 @@ resource "aws_eip""nat_gateway_eip"{
 # https://registry.terraform.io/provdiers/hashicorp/aws/latest/docs/resources/nat_gateway
 
 resource "aws_nat_gateway" "nat" {
-   allocation_id = aws_eip.vpc_eip.allocation_id
-  subnet_id     = aws_subnet.public_b.id
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
 
   tags = {
-    Name = "task ngw"
+    Name = "gw NAT"
   }
-
 }
 
 # # Route Table for Public Subnet

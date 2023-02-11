051edb18238284d6e91161fc6fc23e0f81599d85
h-amini1 <amini.hassan27@gmailcom>
2023-02-11 11:37:19 -0600

    add backend

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,5 +3,9 @@ terraform {
     bucket = "terraform-session-backend-hassan"
     key    = "session-5/terraform.tfstate" # where does terraform need to store you file. Path or
     region = "us-west-1"
+    dynamodb_table = "terraform-session-sep-lock" # Lock Table
   }
 }
+
+# 1. You cannot usee any variables in key
+# 2. 

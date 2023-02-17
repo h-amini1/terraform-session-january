2b15b0144a5143b55123fbaad2c722405c4264f2
h-amini1 <amini.hassan27@gmailcom>
2023-02-16 20:36:11 -0600

    add vpc1.1 file

--- task/vpc1.1/vpc.tf
+++ task/vpc1.1/vpc.tf
@@ -58,12 +58,6 @@ resource "aws_route_table" "public" {
   }
 }
 
-# Association Public Subnet and Pulbic Route Table
-
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public.id
-}
 
 # Create private subnet
 resource "aws_subnet" "private_a" {

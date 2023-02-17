a340a0a054d81fb1ee8b6f933dfe309a7922a9ad
h-amini1 <amini.hassan27@gmailcom>
2023-02-17 11:31:17 -0600

    updated

--- task/vpc1.1/vpc.tf
+++ task/vpc1.1/vpc.tf
@@ -82,8 +82,8 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public_subnets" {
   count          = length(var.Availibity_Zones)
-  subnet_id      = aws_subnet.public_subnets.id, (count.index)
-  route_table_id = aws_route_table.public.id, (count.index )
+  subnet_id      = aws_subnet.public_subnets.id, [count.index]
+  route_table_id = aws_route_table.public.id, [count.index]
 }
 
 resource "aws_route_table" "private" {
@@ -101,6 +101,6 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "private_subnets" {
  # count         = length(var.Availibity_Zones)
-  subnet_id      = aws_subnet.private_subnets.id, ( count.index )
-  route_table_id = aws_route_table.private_subnet.id, ( count.index )
+  subnet_id      = aws_subnet.private_subnets.id, [count.index]
+  route_table_id = aws_route_table.private_subnet.id, [count.index]
 }

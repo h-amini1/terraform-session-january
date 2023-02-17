5934b183f7adda757a0e9d5fa09e44fb7d929a37
h-amini1 <amini.hassan27@gmailcom>
2023-02-16 20:58:30 -0600

    add vpc1.1 file

---
+++ task/vpc1.1/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "main_task" {
+  count = 3      
+  ami           = data.aws_ami.amazon_linux_2.id  
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.main_sg.id ] 
+  tags = {
+    Name = "${var.env}-instance"                      
+}
+
+

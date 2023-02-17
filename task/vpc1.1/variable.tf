6a48b46499fa9f822c3af736712cf99003e25000
h-amini1 <amini.hassan27@gmailcom>
2023-02-16 20:23:52 -0600

    add vpc1.1 file

--- task/vpc1.1/variable.tf
+++ task/vpc1.1/variable.tf
@@ -1,97 +1,14 @@
-# variable for DIDR block and name tag's for VPC
-variable "vpc_cidr" {
+# Variable for Environment
+variable "env" {
   type = string
-  description = "defines CIDR block"
-  default = "10.0.0.0/16"
+  description = "Variable for Environment"
+  default = "dev"
 }
-variable "vpc-main" {
-  type = string
-  description = "give var for vpc"
-  default = "task_vpc"
-}
-
-# variables for CIDR, AZ and tag in public Subnet
-
-variable "vpc_cidr-1a" {
-  type = string
-  description = "defines CIDR block"
-  default = "10.0.1.0/24"
-}
-variable "zone_a" {
-  type = string
-  description = "Defines Availibity Zone"
-  default = "us-west-1a" 
-}
-variable "pub-sub-a" {
-  type = string
-  description = "name tag for sub a"
-  default = "public_sub_a"
-}
-
-#...............................
-
-variable "vpc_cidr-1b" {
-  type = string
-  description = "defines CIDR block"
-  default = "10.0.2.0/24"
-}
-variable "zone_b" {
-  type = string
-  description = "Defines Availibity Zone"
-  default = "us-west-1b" 
-}
-variable "pub-sub-b" {
-  type = string
-  description = "name tag for sub b"
-  default = "public_sub_b"
-}
-
-# variables for CIDR, AZ and tag in private Subnet
-
-variable "cidr-prv-a" {
-  type = string
-  description = "defines CIDR block for private sub"
-  default = "10.0.11.0/24"
-}
-
-variable "prv-sub-a" {
-  type = string
-  description = "name tag for sub b"
-  default = "private_sub_a"
-}
-
-#..........................................
-variable "cidr-prv-b" {
-  type = string
-  description = "defines CIDR block"
-  default = "10.0.12.0/24"
-}
-
-variable "prv-sub-b" {
-  type = string
-  description = "name tag for sub b"
-  default = "prv_sub_b"
-}
-
-# variables CIDR blocks for all traffic
 
 variable "cidr_blocks" {
   type = string
   description = "defines CIDR block for all traffic"
-  default = "0.0.0.0/0"
+  default = ["0.0.0.0/0", "10.0.0.0/16", "10.0.1.0/24", "10.0.2.0/24", "10.0.11.0/24", "10.0.12.0/24"]
 }
 
-# tag name Internet Gateway
 
-variable "IGW" {
-  type = string
-  description = "tag name for igw"
-  default = "Internet_IGW"
-}
-
-# var Elastic IP Tag name
-variable "eip" {
-  type = string
-  description = "tag name for eip"
-  default = "eip"
-}
--- task/vpc1.1/vpc.tf
+++ task/vpc1.1/vpc.tf
@@ -1,105 +1,116 @@
-resource "aws_vpc" "task_vpc" {     
-  cidr_block       = var.vpc_cidr
+resource "aws_vpc" "task_vpc" { 
+  count = 1  
+  cidr_block       = element( var.cidr_block, count.index )
+  instance_tenancy = "default"
+
   tags = {
-    Name = var.vpc-main
+    "Name" = "${var.env}-main_vpc"
   }
 } 
-# Public and private Subnet
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
-
-resource "aws_subnet" "public_a" {
-  vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = var.vpc_cidr-1a 
-  availability_zone = var.zone_a
-  tags = {
-    Name = var.pub-sub-a
-  }
-}
-
-
-
-resource "aws_subnet" "public_b" {
-  vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = var.vpc_cidr-1b
-  availability_zone = var.zone_b
-  tags = {
-    Name = var.pub-sub-b
-  }
-}
-
-resource "aws_subnet" "private_a" {
-  vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = var.cidr-prv-a
-  availability_zone = var.zone_a
-  tags = {
-    Name = var.prv-sub-a
-  }
-}
-
-resource "aws_subnet" "private_b" {
-  vpc_id     = aws_vpc.task_vpc.id
-  cidr_block = var.cidr-prv-b
-  availability_zone = var.zone_b
-  tags = {
-    Name = var.prv-sub-b
-  }
-}
 
 # Main Internet Gateway for VPC
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gatway
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.task_vpc.id
 
   tags = {
-    Name = var.IGW
+    Name = "${var.env}-Internet_IGW"
   }
 }
 
-# Elastic IP for NAT Gatewa
-# https://registry.terraform.io/provdiers/hashicorp/aws/latest/docs/resources/eip
-# Allocate Elastic IP Address
-# Terraform allow elastic ip
+# Public Subnets
+resource "aws_subnet" "public_a" {
+  count = 2
+  vpc_id     = aws_vpc.task_vpc.id
+  cidr_block = element( var.cidr_block, count.index ) 
+  availability_zone = data.aws_availability_zones.available_zones.name[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.env}-pub-sub-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  count = 3
+  vpc_id     = aws_vpc.task_vpc.id
+  cidr_block = element( var.cidr_block, count.index )
+  availability_zone = data.aws_availability_zones.available_zones.name[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.env}-pub-sub-b"
+  }
+}
+
+# Route Table for Public Subnet
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.task_vpc.id
+
+  route {
+    count = 0
+    cidr_block = element( var.cidr_block, count.index )     # Destination is anywhere
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-Public Route Table"
+  }
+}
+
+# Association Public Subnet and Pulbic Route Table
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Create private subnet
+resource "aws_subnet" "private_a" {
+  count = 4
+  vpc_id     = aws_vpc.task_vpc.id
+  cidr_block = element( var.cidr_block, count.index )
+  availability_zone =  data.aws_availability_zones.available_zones.name[0]
+  tags = {
+    Name = "${var.env}-prv-sub-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  count = 5
+  vpc_id     = aws_vpc.task_vpc.id
+  cidr_block = element( var.cidr_block, count.index )
+  availability_zone = data.aws_availability_zones.available_zones.name[1]
+  map_public_ip_on_launch = false      # No public IP address
+  tags = {
+    Name = "${var.env}-prv-sub-b"
+  }
+}
+
+# Elastic IP for NAT Gateway
 
 resource "aws_eip" "nat_eip"{
     vpc        = true
     depends_on = [aws_internet_gateway.igw]
     tags = {
-        Name = var.eip
+        Name = "${var.env}-eip"
     }
 }
 
 
 # Main NAT Gateway for VPC with pulic subnet
-# https://registry.terraform.io/provdiers/hashicorp/aws/latest/docs/resources/nat_gateway
 
 resource "aws_nat_gateway" "nat"{
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_a.id
 
   tags = {
-    Name = "gw NAT"
+    Name = "${var.env}-gw NAT"
   }
 }
 
-# Route Table for Public Subnet
-# https://registry.terraform.io/provdiers/hashicorp/aws/latest/docs/resources/route_table
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.task_vpc.id
-
-  route {
-    cidr_block = var.cidr_blocks
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "Public Route Table"
-  }
-}
 
 # Association between Public Subnet and Pulic Route Table
-# https://registry.terraform.io/provdiers/hashicorp/aws/latest/docs/resources/route_table_association
 
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
@@ -114,15 +125,17 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.task_vpc.id
 
   route {
-    cidr_block = var.cidr_blocks
+    count = 0
+    cidr_block = element( var.cidr_block, count.index )
     nat_gateway_id = aws_nat_gateway.nat.id
   }
   tags = {
-    Name = "Private"
+    Name = "${var.env}-private"
   }
 }
+
+
 # Association betwen Private Subnet and Private Route Table
-# https://registry.terraform.io/provdiers/hashicorp/aws/latest/docs/resources/route_table_association
 
 resource "aws_route_table_association" "private_a" {
   subnet_id      = aws_subnet.private_a.id

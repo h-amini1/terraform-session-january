949d082069b2a2a88608ec6ba7769a7fb78d5c74
h-amini1 <amini.hassan27@gmailcom>
2023-02-16 20:29:18 -0600

    add vpc1.1 file

--- task/vpc1.1/variable.tf
+++ task/vpc1.1/variable.tf
@@ -6,7 +6,7 @@ variable "env" {
 }
 
 variable "cidr_blocks" {
-  type = string
+  type = list(string)
   description = "defines CIDR block for all traffic"
   default = ["0.0.0.0/0", "10.0.0.0/16", "10.0.1.0/24", "10.0.2.0/24", "10.0.11.0/24", "10.0.12.0/24"]
 }

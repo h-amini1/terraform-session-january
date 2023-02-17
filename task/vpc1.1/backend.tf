70f9daa6fb2409070e5dfc913bf534350a442828
h-amini1 <amini.hassan27@gmailcom>
2023-02-17 11:05:34 -0600

    updated

--- task/vpc1.1/backend.tf
+++ task/vpc1.1/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "terraform-session-backend-hassan"
-    key    = "task/vpc1.1/terraform.tfstate" # where does terraform need to store you file. Path or
+    key    = "task/vpc1.1/terraform.tfstate" 
     region = "us-west-1"
-    dynamodb_table = "terraform-session-sep-lock" # Lock Table
+    dynamodb_table = "terraform-session-sep-lock" 
   }
 }
 

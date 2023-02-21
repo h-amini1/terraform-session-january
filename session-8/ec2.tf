f6659cff40163c161ead3aaaea70a55de10b84cb
h-amini1 <amini.hassan27@gmailcom>
2023-02-20 19:09:05 -0600

    ff

--- session-8/ec2.tf
+++ session-8/ec2.tf
@@ -4,7 +4,19 @@ resource "aws_instance" "main" {
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.main_sg.id ] # a list of strings
   key_name = aws_key_pair.terraform_server.key_name
+  provisioner "file" {
+    source = "/home/ec2-user/terraform-session-january/session-8/index.html"                # path on Terraform server
+    destination = "/tmp/index.html"               #path for the Remote server
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"      #Remote server user
+      host = self.public_ip     # self means all this block
+      provate_key = file("~/.ssh/id_rsa")
+    }
+  }
 }
+# ssh user@ip it automatically checks public key on 
 
 resource "aws_key_pair" "terraform_server" {
   key_name = "Terraform-Server-key"

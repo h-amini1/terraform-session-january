5e4bc83e587aee89e8abb805eb39b23d7e006b8d
h-amini1 <amini.hassan27@gmailcom>
2023-02-22 20:12:38 -0600

    update module

--- session-9/dev/main.tf
+++ session-9/dev/main.tf
@@ -12,5 +12,5 @@ module "ec2_instance" {
   ################### Variables #################
   env = "dev"
   ami = "ami-0036b4598ccd42565"
-  instinstance_type = "t2.micro"
+  instance_type = "t2.micro"
 }

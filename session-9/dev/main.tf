f434e61677a9cad2e06c9779b13772411e5c55b5
h-amini1 <amini.hassan27@gmailcom>
2023-02-22 20:38:24 -0600

    ff

--- session-9/dev/main.tf
+++ session-9/dev/main.tf
@@ -13,9 +13,12 @@ module "ec2_instance" {
   env = "dev"
   ami = "ami-0036b4598ccd42565"
   instance_type = "t2.micro"
+  sg = [module.ec2_sg.id]    # Module.Module_Name.Attribute
 }
 
 module "ec2-sg" {
   source = "../../modules/sg"
   env = "dev"
 }
+
+# Reference to child module

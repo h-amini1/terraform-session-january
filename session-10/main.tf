fe7459cfa4ebde21e511200200628cd404227258
h-amini1 <amini.hassan27@gmailcom>
2023-02-25 12:11:21 -0600

    calling folder

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-    source = "github.com/h-amini1/terraform-session-january/main/modules/ec2" 
+    source = "github.com/h-amini1/terraform-session-january/modules/ec2" 
     env = "dev"
     ami = "ami-0036b4598ccd42565"
     instance_type = "t2.micro"

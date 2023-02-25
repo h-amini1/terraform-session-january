b110344ee84721c4d1719f7f8f8af06bd857a2a7
h-amini1 <amini.hassan27@gmailcom>
2023-02-25 12:27:23 -0600

    calling folder

--- session-9/dev/backend.tf
+++ session-9/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-session-backend-hassan"
-    key    = "session-8/terraform.tfstate" # where does terraform need to store you file. Path or prefix
+    key    = "session-9/terraform.tfstate" # where does terraform need to store you file. Path or prefix
     region = "us-west-1"
     dynamodb_table = "terraform-session-sep-lock" # Lock Table
   }

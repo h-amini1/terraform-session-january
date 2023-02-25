437b8e15b4fabd4298c393ba456eab65c50e7bca
h-amini1 <amini.hassan27@gmailcom>
2023-02-25 12:57:34 -0600

    add Privision instructions

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,6 +1,15 @@
+# Calling child module
+
+# Git hub
 module "ec2_instance" {
     source = "github.com/h-amini1/terraform-session-january//modules/ec2?ref=v1.0.1" 
     env = "dev"
     ami = "ami-0036b4598ccd42565"
     instance_type = "t2.micro"
+}
+
+# Terraform Reposotyoy 
+module "vpc" {
+  source = "terraform-aws-module/vpc/aws"
+  version = "3.18.0"
 }
